44cf8aac6ccc4886ccd37873c708ade39dc57b7c
nagirikatakampraneeth07 <163383487+nagirikatakampraneeth07@users.noreply.github.com>
2024-12-05 18:41:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-00db8dadb36c9815e"
-    instance_type = "t2.nano"
-    tags = {
-      Name = "test"
-    }
+resource "aws_s3_bucket" "thala" {
+  bucket = "thalaforareason0424"
 }
